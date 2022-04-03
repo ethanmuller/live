3f583bc6c65551da4a426d31262c2d0874d79cd2
Ethan Muller <hi@emu.media>
2022-04-02 21:28:48 -0400

    wip: differentiate between me editing and somebody else editing
    blue box = you are editing
    gray box = somebody else is editing

--- components/WordSelector.vue
+++ components/WordSelector.vue
@@ -1,13 +1,14 @@
 <template>
-  <span>
+  <span style="position: relative;">
     <span v-bind:class="{
     'word-selector': true,
     'word-selector--is-being-edited': isBeingEdited,
+    'word-selector--is-being-edited-by-me': isBeingEditedByMe,
     }">
-      <button v-if="!isLocked && !isBeingEdited" @click="editBlank"> {{ underscores }} </button>
-      <span v-else-if="isBeingEdited">{{ word || underscores }}</span>
+      <button v-if="!isLocked && !isBeingEdited" @click="openWordList"> {{ underscores }} </button>
       <span v-else>{{ word || underscores }}</span>
     </span>
+    <div v-if="isBeingEditedByMe" class="word-list">ayye</div>
   </span>
 </template>
 
@@ -18,6 +19,7 @@ export default {
     return {
       word: "",
       pastWords: [],
+      isWordListOpen: false,
     }
   },
   watch: {
@@ -29,17 +31,30 @@ export default {
     "underscores": function() {
       return "＿".repeat(parseInt(this.length, 10))
     },
-    "isBeingEdited": function() {
+    "isBeingEditedByMe": function() {
+      // todo: move this method out of computed property.
+      // maybe calculate index on mount instead?
       const i = this.findIndexOfInstance(this)
-      console.log(this.blankList[i] && this.blankList[i].length)
+      return this.blankList[i] === this.socket.id
+    },
+    "isBeingEdited": function() {
+      // todo: move this method out of computed property.
+      // maybe calculate index on mount instead?
+      const i = this.findIndexOfInstance(this)
       return this.blankList[i] && this.blankList[i].length === 20
     },
   },
   methods: {
     editBlank() {
       const i = this.findIndexOfInstance(this)
-      console.log('editing')
       this.socket.emit('open word selector', i)
+    },
+    openWordList() {
+      this.isWordListOpen = true
+      this.editBlank()
+    },
+    closeWordList() {
+      this.isWordListOpen = false
     },
     isWordUsed(word) {
       return this.blankList.indexOf(word) > -1
@@ -58,7 +73,6 @@ export default {
       const i = this.findIndexOfInstance(this)
       const to = this.word
       const from = this.pastWords[0]
-      console.log('submitting word', this.word)
 
       this.socket.emit('submit word', to, from, i, (err) => {
         if (err) {
@@ -82,6 +96,7 @@ export default {
     display: inline-block;
     transition: all 250ms ease-out;
     opacity: 1;
+    line-height: 1;
   }
 
   @keyframes blink {
@@ -96,6 +111,9 @@ export default {
     background: #aaa;
     color: transparent;
     animation: blink 500ms infinite alternate;
+  }
+  .word-selector--is-being-edited-by-me {
+    background: #54f;
   }
   @supports (-webkit-touch-callout: none) {
     /* CSS specific to iOS devices */
@@ -138,4 +156,17 @@ export default {
   option[disabled] {
     color: #ddd;
   }
+
+
+  .word-list {
+    background: #FFFFFF;
+    box-shadow: 0px 100px 80px rgba(0, 0, 0, 0.09), 0px 33.2436px 29.2013px rgba(0, 0, 0, 0.0696101), 0px 5.32773px 14.1767px rgba(0, 0, 0, 0.0500626), 0px -6.98175px 6.94968px rgba(0, 0, 0, 0.0313772), 0px -8.15691px 2.74791px rgba(0, 0, 0, 0.013912);
+    border-radius: 7px;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    z-index: 1;
+  }
+
+
 </style>
