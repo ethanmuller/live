0725d24ffefd71b07e8b0499ed9a5c67cc77e6bf
Ethan Muller <ethanbmuller@gmail.com>
2022-04-09 15:42:56 -0400

    wip: static

--- pages/index.vue
+++ pages/index.vue
@@ -1,100 +1,62 @@
 <template>
-<main class="main main--home">
-  
-  <div class="join-chunk">
-    <div v-if="parties.length < 1" style="padding: 1.5rem;"">Nothing is happening here right now. Please try refreshing your browser.</div>
-    <div v-else>
-      active parties:
-      <ul class="list-of-parties">
-        <li v-for="party in parties">
-          <NuxtLink :to="{ path: `/party/${party.id}`, query: $route.query }">{{party.id}}</NuxtLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div class="host-note mod-panel" v-if="$route.query.role === 'mod'">
-      <span class="mod-panel__title">Mod Panel</span>
-    <form @submit.prevent="createParty">
-        <button class="btn btn--sm btn--mod">Create Party</button>
-      </form>
-    </div>
-</main>
+<canvas ref="canvas"></canvas>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      hostParty: {},
-      hostPartyTicket: '',
-      partyCode: '',
-      selectedGame: 'poem',
-      parties: [],
+      canvas: null,
+      ctx: null,
+      width: null,
+      height: null,
     }
   },
-  
   mounted() {
-    this.hostPartyTicket = Math.floor(Math.random() * 1000000)
+    this.canvas = this.$refs.canvas;
+
+    this.ctx = this.canvas.getContext('2d');
+    this.setSize(this.canvas)
+
+
+    this.draw()
+
+    window.addEventListener('resize', this.setSize)
+    window.requestAnimationFrame(this.draw)
   },
-
-  async fetch() {
-    this.parties = await fetch('/api/party', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }).then(res => res.json())
-
-    if (this.parties.length === 1 && this.$route.query.role !== 'mod') {
-      this.$router.push({
-        path: `/party/${this.parties[0].id}`,
-        query: this.$route.query,
-      })
-    }
-  },
-
   methods: {
-    async createParty() {
-        const data = {
-            ticket: this.hostPartyTicket,
-            selectedGame: this.selectedGame,
-        }
-      
-      const response = await fetch('/api/party', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data)
-      })
-      
-      const responseJSON = await response.json()
-      
-      this.$router.push({
-        name: 'party-party',
-        params: {
-          party: responseJSON.id,
-          role: 'host',
-          ticket: this.hostPartyTicket,
-        },
-        query: {
-          role: 'mod',
-        },
-      })
-      // $router.push({name: 'next-page', params: {foo: 1}})
+    draw() {
+      this.ctx.fillRect(0, 0, this.width, this.height);
+      const imageData = this.ctx.getImageData(0, 0, this.width, this.height)
+      const data = imageData.data
+
+      for (let i = 0; i < data.length; i += 4) {
+        const c = Math.random()*255
+        data[i]     = c;
+        data[i + 1] = c;
+        data[i + 2] = c;
+      }
+      this.ctx.putImageData(imageData, 0, 0);
+
+      window.requestAnimationFrame(this.draw)
     },
-    joinParty() {
-      this.$router.push({
-        name: 'party-party',
-        params: {
-          party: this.partyCode,
-          role: 'guest',
-        },
-      })
-    }
+    setSize() {
+      this.width = window.innerWidth * 0.25;
+      this.height = window.innerHeight * 0.25;
+      this.canvas.width = this.width
+      this.canvas.height = this.height
+    },
   },
 }
 </script>
 
 <style>
+canvas {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  image-rendering: pixelated;
+}
 </style>
