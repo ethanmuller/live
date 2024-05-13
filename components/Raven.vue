d35eab836a44c22a66d3494b5322410022c3e5b5
Ferran <fonserbc@gmail.com>
2024-05-13 11:15:11 +0800

    Revert "Removing bass"
    This reverts commit 1f7a4936f9126d849c835d89f0346b308eb88288.

--- components/Raven.vue
+++ components/Raven.vue
@@ -98,7 +98,7 @@ export default {
       displayText: {
         Piano: "Piano 🎹",
         Guitar: "Guitar 🎸",
-        /*Bass: "Bass 🎻",*/
+        Bass: "Bass 🎻",
         Drums: "Drums 🥁"
       }
       // Only one WordSelector is allowed open at a time.
